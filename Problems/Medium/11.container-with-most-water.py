050eb60031cd6fb92ed4aee78002f6f887753b04
ryderfang <ryderfang@tencent.com>
2021-05-08 16:56:25 +0800

    [20210508] p11: opt, O(n)

--- Problems/Medium/11.container-with-most-water.py
+++ Problems/Medium/11.container-with-most-water.py
@@ -10,59 +10,23 @@ from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         ans = 0
-        sz = len(height)
-        # f[i] is the first one which height[f[i]] > height[i] and f[i] > i
-        f = [0 for i in range(sz)]
-        # b[i] is the first one which height[b[i]] > height[i] and b[i] < i
-        b = [-1 for i in range(sz)]
-        for j in range(sz-1, 0, -1):
-            if height[j] >= height[0]:
-                f[0] = j
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            dist = right - left
+            if ans > (10 ** 4) * dist:
                 break
-        if f[0] > 0:
-            ans = max(ans, height[0] * (f[0] - 0))
-            # print(ans)
-        for i in range(1, sz):
-            if height[i] >= height[i-1]:
-                for j in range(f[i-1], i, -1):
-                    if height[j] >= height[i]:
-                        f[i] = j
-                        break
-                for k in range(b[i-1] if b[i-1] >= 0 else i, i):
-                    if height[k] >= height[i]:
-                        b[i] = k
-                        break
+            ans = max(ans, min(height[left], height[right]) * dist)
+            # 这种情况 area([left, right]) > area([left+1, right])
+            if height[right] < height[left]:
+                right -= 1
             else:
-                for j in range(sz - 1, f[i-1] - 1, -1):
-                    if height[j] >= height[i]:
-                        f[i] = j
-                        break
-                for k in range(0, b[i-1] + 1 if b[i-1] >= 0 else i):
-                    if height[k] >= height[i]:
-                        b[i] = k
-                        break
-            if f[i] > i:
-                tmp = height[i] * (f[i] - i)
-                if ans < tmp:
-                    # print(tmp, height[i], i, f[i])
-                    ans = tmp
-            if b[i] >= 0 and b[i] < i:
-                tmp = height[i] * (i - b[i])
-                if ans < tmp:
-                    # print(tmp, height[i], b[i], i)
-                    ans = tmp
+                left += 1
         return ans
 
 # @lc code=end
 
 # @lc comment
 """
-在 (i, sz-1] 范围内找第一个 height[j] >= height[i] 的 j，记为 f[i] = j
-在 [0, i) 范围内找第一个 height[j] >= height[i] 的 j，记为 b[i] = j
-如果 height[i+1] >= height[i]，则必然 height[i+1] >= height[i] > height[f[i] + 1]，所以
-f[i+1] 只可能在 [i+1, f[i]] 之间；如果 height[i+1] < height[i]，则只可能在 [f[i], sz-1] 之间
-同理 height[i+1] >= height[i]，b[i+1] -> [b[i], i+1]
-height[i+1] < height[i], b[i+1] -> [0, b[i]]
+两边向中间逼近，移动 height 小的才有可能获得更大面积的机会。
 """
-
-
