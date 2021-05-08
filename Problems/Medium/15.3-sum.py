76892953b40d177e12861fd3008db135debfe9ef
ryderfang <ryderfang@tencent.com>
2021-05-08 18:11:46 +0800

    [20210508] p15: todo

---
+++ Problems/Medium/15.3-sum.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        
+        
+# @lc code=end
+
--- Problems/main.py
+++ Problems/main.py
@@ -2,7 +2,7 @@ import importlib.util
 import inspect
 import glob, os
 
-PROBLEM_NO = 12
+PROBLEM_NO = 15
 PROBLEM_LV = '/Medium/'
 
 os.chdir(os.path.dirname(__file__))
@@ -20,9 +20,4 @@ for member in inspect.getmembers(module):
 
 if __name__ == "__main__":
     sol = cls()
-    print(sol.intToRoman(3))
-    print(sol.intToRoman(4))
-    print(sol.intToRoman(9))
-    print(sol.intToRoman(58))
-    print(sol.intToRoman(1994))
-    print(sol.intToRoman(2998))
+    print(sol.threeSum([1]))
