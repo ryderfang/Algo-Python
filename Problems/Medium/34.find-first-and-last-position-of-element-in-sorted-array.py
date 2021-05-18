7834a27629206fc461c5f1c5167a99a90b543be4
ryderfang <ryderfang@tencent.com>
2021-05-18 23:19:47 +0800

    [20210518] p34: opt

--- Problems/Medium/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ Problems/Medium/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,28 +9,31 @@ from typing import List
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         sz = len(nums)
-        if sz == 0:
-            return [-1, -1]
+        ans = [-1, -1]
         left = 0
         right = sz - 1
-        while left < right and nums[left] < nums[right]:
-            if nums[left] == target and nums[right] == target:
-                break
+        # 要有等于
+        while left <= right:
             mid = (left + right) // 2
             if nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
             else:
-                if nums[left] < target:
-                    left += 1
-                if nums[right] > target:
-                    right -= 1
-                
-        if nums[left] != target:
-            return [-1, -1]
-        else:
-            return [left, right]
+                right = mid -1
+        if left >= sz or nums[left] != target:
+            return ans
+        
+        ans[0] = left
+        right = sz - 1
+        # 要有等于
+        while left <= right:
+            mid = (left + right) // 2
+            # 区别
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid -1
+        ans[1] = right
+        return ans
         
 # @lc code=end
 
