7e2d2972a0dee0ccc2332156a246ed61f28b8bd0
ryderfang <ryderfang@tencent.com>
2021-05-25 23:39:29 +0800

    [20210525] p39: opt

--- Problems/Medium/39.combination-sum.py
+++ Problems/Medium/39.combination-sum.py
@@ -8,25 +8,35 @@
 from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        sz = len(candidates)
-        if sz == 0:
-            return []
-        x = candidates[0]
-        if sz == 1:
-            if target % x == 0:
-                return [[x] * (target // x)]
-            else:
+        def getAns(candidates, target):
+            sz = len(candidates)
+            if sz == 0:
                 return []
-        cnt = 0
-        ans = []
-        while x * cnt <= target:
-            left = target - x * cnt
-            other = self.combinationSum(candidates[1:], left)
-            if len(other) > 0:
-                for t in other:
-                    ans.append([x] * cnt + t)
-            cnt += 1
-        return ans
+            x = candidates[0]
+            if sz == 1:
+                if target % x == 0:
+                    return [[x] * (target // x)]
+                else:
+                    return []
+            # opt
+            if x > target:
+                return []
+            cnt = 0
+            ans = []
+            while x * cnt <= target:
+                left = target - x * cnt
+                if left == 0:
+                    ans.append([x] * cnt)
+                    break
+                other = getAns(candidates[1:], left)
+                if len(other) > 0:
+                    for t in other:
+                        ans.append([x] * cnt + t)
+                cnt += 1
+            return ans
+        # opt, sort
+        candidates.sort()
+        return getAns(candidates, target)
 
 # @lc code=end
 
