e328000aa6e788aef07ce62f4a4aa8c89811e231
ryderfang <ryderfang@tencent.com>
2021-06-19 18:58:08 +0800

    [20210619] p55: simplest

--- Problems/Medium/55.jump-game.py
+++ Problems/Medium/55.jump-game.py
@@ -8,14 +8,11 @@
 from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        sz = len(nums)
         furthest = 0 
-        for i, n in enumerate(nums[0:sz-1]):
-            if i <= furthest < i + n:
-                furthest = i + n
-            # opt
-            if furthest >= sz - 1:
-                break
-        return True if (furthest >= sz - 1) else False
+        for i, n in enumerate(nums):
+            if i > furthest:
+                return False
+            furthest = max(furthest, i + n)
+        return True
 # @lc code=end
 
