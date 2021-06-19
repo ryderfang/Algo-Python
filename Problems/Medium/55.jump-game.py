8300769fdbd0357d3e34f825e11691700be4645c
ryderfang <ryderfang@tencent.com>
2021-06-19 18:55:16 +0800

    [20210619] p55: opt

--- Problems/Medium/55.jump-game.py
+++ Problems/Medium/55.jump-game.py
@@ -9,19 +9,13 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         sz = len(nums)
-        ans = [0] * sz
-        ans[0] = 1
         furthest = 0 
         for i, n in enumerate(nums[0:sz-1]):
-            # opt
-            if not ans[i]:
-                break
-            if i + n > furthest:
-                ans[furthest+1:i+n+1] = [ans[i]] * (i+n-furthest)
+            if i <= furthest < i + n:
                 furthest = i + n
             # opt
-            if furthest > sz:
+            if furthest >= sz - 1:
                 break
-        return True if ans[-1] else False
+        return True if (furthest >= sz - 1) else False
 # @lc code=end
 
